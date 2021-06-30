88c4f0b80612c8e3b412115ec1bfcc1b2714266f
Nedyalko Andreev <n@andreev.sh>
2021-06-30 23:51:44 +0300

    Add a PoC for lazily parsing nested JSON options

--- source_json.go
+++ source_json.go
@@ -6,30 +6,30 @@ import (
 	"fmt"
 )
 
+// TODO: use json.Decoder for this? json.Unmarshal() is a bit too magical
+
+// TODO: rename this to something else? e.g. JSONDocument?
 type SourceJSON struct {
-	data   []byte
 	fields map[string]json.RawMessage
-	// TODO: I'm thinking that when this receives a JSON file, it should parse
-	// it to a map[string]json.RawMessage. Then, it can parse every
-	// json.RawMessage on demand (i.e. lazily), to the type specified by its
-	// `name` (set in `From()` below)
+	init   func() error
 }
 
 func NewJSONSource(data []byte) *SourceJSON {
+	fields := make(map[string]json.RawMessage)
 	return &SourceJSON{
-		data:   data,
-		fields: make(map[string]json.RawMessage),
+		fields: fields,
+		init: func() error {
+			// TODO: differentiate between an empty data and no data (nil)?
+			if len(data) == 0 {
+				return nil
+			}
+			return json.Unmarshal(data, &fields) // TODO: better error message
+		},
 	}
 }
 
 func (sj *SourceJSON) Initialize() error {
-	if len(sj.data) > 0 {
-		// TODO: differentiate between an empty data and no data (nil)?
-		if err := json.Unmarshal(sj.data, &sj.fields); err != nil {
-			return err
-		}
-	}
-	return nil
+	return sj.init()
 }
 
 func (sj *SourceJSON) GetName() string {
@@ -45,11 +45,20 @@ func (sj *SourceJSON) From(name string) *jsonBinding {
 	return &jsonBinding{
 		source: sj,
 		name:   name,
+		lookup: func() (json.RawMessage, error) {
+			raw, ok := sj.Lookup(name)
+			if !ok {
+				return nil, ErrorMissing // TODO: better error message, e.g. 'field %s is not present in %s'?
+			}
+			return raw, nil
+		},
 	}
 }
 
+// TODO: export and rename? e.g. to JSONProperty?
 type jsonBinding struct {
-	source *SourceJSON
+	source Source
+	lookup func() (json.RawMessage, error)
 	name   string
 }
 
@@ -57,11 +66,36 @@ func (jb *jsonBinding) GetSource() Source {
 	return jb.source
 }
 
+func (jb *jsonBinding) From(name string) *jsonBinding {
+	return &jsonBinding{
+		source: jb.source,
+		name:   jb.name + "." + name,
+		lookup: func() (json.RawMessage, error) {
+			raw, err := jb.lookup()
+			if err != nil {
+				return nil, err
+			}
+
+			// TODO: cache this, so we don't parse sub-configs multiple times
+			subdoc := NewJSONSource(raw)
+			if err := subdoc.init(); err != nil {
+				return nil, err
+			}
+
+			rawEl, ok := subdoc.Lookup(name)
+			if !ok {
+				return nil, ErrorMissing // TODO: better error message, e.g. 'field %s is not present in %s'?
+			}
+			return rawEl, nil
+		},
+	}
+}
+
 func (jb *jsonBinding) BindStringValueTo(dest *string) func() error {
 	return func() error {
-		raw, ok := jb.source.fields[jb.name]
-		if !ok {
-			return ErrorMissing // TODO: better error message, e.g. 'field %s is not present in %s'?
+		raw, err := jb.lookup()
+		if err != nil {
+			return err
 		}
 
 		return json.Unmarshal(raw, dest) // TODO: less reflection, better error messages
@@ -70,9 +104,9 @@ func (jb *jsonBinding) BindStringValueTo(dest *string) func() error {
 
 func (jb *jsonBinding) BindInt64ValueTo(dest *int64) func() error {
 	return func() error {
-		raw, ok := jb.source.fields[jb.name]
-		if !ok {
-			return ErrorMissing // TODO: better error message, e.g. 'field %s is not present in %s'?
+		raw, err := jb.lookup()
+		if err != nil {
+			return err
 		}
 
 		return json.Unmarshal(raw, dest) // TODO: less reflection, better error messages
@@ -81,9 +115,9 @@ func (jb *jsonBinding) BindInt64ValueTo(dest *int64) func() error {
 
 func (jb *jsonBinding) BindTextBasedValueTo(dest encoding.TextUnmarshaler) func() error {
 	return func() error {
-		raw, ok := jb.source.fields[jb.name]
-		if !ok {
-			return ErrorMissing // TODO: better error message, e.g. 'field %s is not present in %s'?
+		raw, err := jb.lookup()
+		if err != nil {
+			return err
 		}
 
 		// Progressive enhancement ¯\_(ツ)_/¯ If the destination supports directly
@@ -113,9 +147,9 @@ type jsonBindingWithDest struct {
 
 func (jbd *jsonBindingWithDest) BindValue() func() error {
 	return func() error {
-		raw, ok := jbd.source.fields[jbd.name]
-		if !ok {
-			return ErrorMissing // TODO: better error message, e.g. 'field %s is not present in %s'?
+		raw, err := jbd.lookup()
+		if err != nil {
+			return err
 		}
 
 		return jbd.dest.UnmarshalJSON(raw)
